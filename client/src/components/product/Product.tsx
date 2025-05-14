b3888636354b6856b2941ec2d9f3592f6543f8b5
EmpireStatePavlik <zzzlatan9@mail.ru>
2025-05-14 22:38:12 +0300

    save1

---
+++ client/src/components/product/Product.tsx
@@ -0,0 +1,63 @@
+import type { IProductType } from './type';
+import React from 'react';
+
+
+function Product( { product }: { product: IProductType[] }): React.JSX.Element {
+
+
+    return (
+        <section>
+            <h1>Доступное меню</h1>
+                <div>
+                    <div>Название: {product.name}</div>
+                    <div>Картинка: {product.imgURL} </div>
+                    <div>Исходная цена: {product.price} </div>
+                    <div>Скидка: {product.discount} </div>
+                    <div>Цена со скидкой: {product.endPrice} </div>
+                    <button onClick={() => handleClick(product.id)}></button>
+                </div>
+        </section>
+    )       
+
+}
+
+export default Product;
+
+
+
+
+// const product = 
+//     {
+//         "id": 16,
+//         "name": "Гамбургер",
+//         "imgURL": null,
+//         "price": 450,
+//         "discount": 15,
+//         "courier_id": 5,
+//         "createdAt": "2025-05-14T17:56:24.073Z",
+//         "endPrice": "383"
+//     }
+
+
+// const Data = [
+//     {
+//         "id": 16,
+//         "name": "Гамбургер",
+//         "imgURL": null,
+//         "price": 450,
+//         "discount": 15,
+//         "courier_id": 5,
+//         "createdAt": "2025-05-14T17:56:24.073Z",
+//         "endPrice": "383"
+//     },
+//     {
+//         "id": 10,
+//         "name": "Лазанья",
+//         "imgURL": null,
+//         "price": 650,
+//         "discount": 15,
+//         "courier_id": 5,
+//         "createdAt": "2025-05-14T17:56:24.073Z",
+//         "endPrice": "553"
+//     },
+// ]
