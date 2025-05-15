d02f211f76891ca28eb1be5ad214e8eaca68d656
EmpireStatePavlik <zzzlatan9@mail.ru>
2025-05-15 23:44:53 +0300

    product card and Products page completed

--- client/src/components/product/Product.tsx
+++ client/src/components/product/Product.tsx
@@ -1,67 +1,27 @@
-import React from 'react';
-import type { IProductType } from './type';
+import React from "react";
+import type { IProductType } from "./type";
 
 interface ProductProps {
-    product: IProductType,
-    handleClick: (id:number) => void
+  product: IProductType;
+  handleClick: (id: number) => void;
 }
 
-function Product( { product, handleClick }: ProductProps): React.JSX.Element {
-
-
-    return (
-        <section>
-            <h1>Доступное меню</h1>
-                <div>
-                    <div>Название: {product.name}</div>
-                    <div>Картинка: {product.imgURL} </div>
-                    <div>Исходная цена: {product.price} </div>
-                    <div>Скидка: {product.discount} </div>
-                    <div>Цена со скидкой: {product.endPrice} </div>
-                    <button onClick={() => handleClick(product.id)}></button>
-                </div>
-        </section>
-    )       
-
+function Product({ product, handleClick }: ProductProps): React.JSX.Element {
+  return (
+    <section>
+      <h1>Выбирай</h1>
+      <div>
+        <div>Название: {product.name}</div>
+        <div>Картинка: {product.imgURL} </div>
+        <div>Исходная цена: {product.price} </div>
+        <div>Скидка: {product.discount} </div>
+        <div>Цена со скидкой: {product.endPrice} </div>
+        <button onClick={() => handleClick(product.id)}>
+          Добавить в корзину
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Product;
-
-
-
-
-// const product = 
-//     {
-//         "id": 16,
-//         "name": "Гамбургер",
-//         "imgURL": null,
-//         "price": 450,
-//         "discount": 15,
-//         "courier_id": 5,
-//         "createdAt": "2025-05-14T17:56:24.073Z",
-//         "endPrice": "383"
-//     }
-
-
-// const Data = [
-//     {
-//         "id": 16,
-//         "name": "Гамбургер",
-//         "imgURL": null,
-//         "price": 450,
-//         "discount": 15,
-//         "courier_id": 5,
-//         "createdAt": "2025-05-14T17:56:24.073Z",
-//         "endPrice": "383"
-//     },
-//     {
-//         "id": 10,
-//         "name": "Лазанья",
-//         "imgURL": null,
-//         "price": 650,
-//         "discount": 15,
-//         "courier_id": 5,
-//         "createdAt": "2025-05-14T17:56:24.073Z",
-//         "endPrice": "553"
-//     },
-// ]
--- client/src/pages/products/Products.tsx
+++ client/src/pages/products/Products.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import $api from "../../configs/axiosConfig";
+import Product from "../../components/product/Product";
+import type { IProductType } from "../../components/product/type";
 
-function Products() {
+function Products(): React.JSX.Element {
+  const [products, setProducts] = useState<IProductType[] | []>([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts(): Promise<void> {
+    const response = await $api.get("/products");
+    if (response.status === 200) {
+      setProducts(response.data);
+    }
+  }
+
+  async function handleClick(id: number): Promise<void> {
+    await $api.post("/busket", { product_id: id });
+    alert("Товар добавлен в корзину!");
+  }
   return (
-    <div>Products</div>
-  )
+    <>
+      <div>Products</div>
+      <div>
+        {products.map((p) => {
+          return <Product product={p} handleClick={handleClick} />;
+        })}
+      </div>
+    </>
+  );
 }
 
-export default Products
+export default Products;
