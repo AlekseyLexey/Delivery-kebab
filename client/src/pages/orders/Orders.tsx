8aa51a97da352c9e17405269dac80348aa6fba1e
EmpireStatePavlik <zzzlatan9@mail.ru>
2025-05-16 22:35:56 +0300

    orders of client

--- client/src/pages/orders/Orders.tsx
+++ client/src/pages/orders/Orders.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import $api from '../../configs/axiosConfig';
+import type { IProductType } from '../../components/product/type';
+import Product from '../../components/product/Product';
 
-function Orders() {
+function Orders(): React.JSX.Element {
+
+const [ orders, setOrders ] = useState([]);
+
+useEffect(() => {
+  getOrdersFromBusket();
+}, [])
+
+async function getOrdersFromBusket(): Promise<void> {
+  const response = await $api.get('/orders');
+  if(response.status === 200) {
+    setOrders(response.data);
+  }
+}
+
+
   return (
+    <>
     <div>Orders</div>
+    <div>
+      {orders.map((p:IProductType) => {
+        return (
+          <li key={p.id}>
+            <h1>Заказ</h1>
+            <Product product={p} />
+          </li>
+        )
+      })}
+    </div>
+    </>
+    
   )
 }
 
